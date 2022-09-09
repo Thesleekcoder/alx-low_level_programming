d8138e07cd01c4081cdf33be74620060827c6b72
Israel <alaladebamigboye@gmail.com>
2022-09-08 22:22:22 -0700

    New update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  *
- *  *main- The entry point of the program
+ *  * Main- The entry point of the program
  *
  *  *Return:if no error return zero, otherwise return non zero value
  *
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art.\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art.\n");
+return(0);
 }
