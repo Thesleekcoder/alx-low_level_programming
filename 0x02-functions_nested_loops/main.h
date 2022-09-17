7a1f7f97bfbc06b98f61c2b65881f3cd31b71781
Israel <alaladebamigboye@gmail.com>
2022-09-17 01:39:01 -0700

    Print Putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,6 @@
-_putchar.c
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
-
-
-
-main.h
-
-
+#include <stdio.h>
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
