24e48ae9bf94460d61adb8305cbf308858711735
Israel <alaladebamigboye@gmail.com>
2022-09-17 23:48:06 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: exit of the main program
+ */
+int main(void)
+{
+	int i;
+	long a = 1, b = 2, c;
+	long sum = 2;
+
+	for (i = 0; i < 30; i++)
+	{
+		c = a + b;
+		if ((c % 2) == 0)
+			sum += c;
+		a = b;
+		b = c;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
