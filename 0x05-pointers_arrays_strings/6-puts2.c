73ac6c1290ea8b68ee74a363e5d626d763dbbf05
Israel <alaladebamigboye@gmail.com>
2022-09-19 20:51:36 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Function prints half of a string
+ * @str: The string to use
+ *
+ */
+void puts_half(char *str)
+{
+	int size = 0;
+	int n;
+
+	for (; str[size] != '\0'; size++)
+	{}
+	if (size % 2 == 0)
+	{
+		n = size / 2;
+	}
+	else
+	{
+		n = (size + 1) / 2;
+	}
+
+	for (; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
