7632d64becf02df6cd7355993972f01dc469024c
Israel <alaladebamigboye@gmail.com>
2022-10-12 19:23:06 -0700

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <main.h>
 
 /**
  * word_count - function counts amount of words
@@ -11,20 +11,20 @@
  */
 int word_count(char *s)
 {
-	int c = 0, i = 0;
+        int c = 0, i = 0;
 
-	while (s[i])
-	{
-		if (s[i] != ' ')
-		{
-			c++;
-			while (s[i] && s[i] != ' ')
-				i++;
-		}
-		else
-			i++;
-	}
-	return (c);
+        while (s[i])
+        {
+                if (s[i] != ' ')
+                {
+                        c++;
+                        while (s[i] && s[i] != ' ')
+                                i++;
+                }
+                else
+                        i++;
+        }
+        return (c);
 }
 
 /**
@@ -35,40 +35,18 @@ int word_count(char *s)
  */
 char **strtow(char *str)
 {
-	char **a, *tmpP;
+        char **a, *tmpP;
 char i = 0, j = 0, pos = 0, size = 0, words = 0, tmp;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	words = word_count(str);
-	if (words == 0)
-		return (NULL);
-	a = (char **)malloc(sizeof(char *) * (words + 1));
-	if (!a)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < words; i++)
-	{
-		size = 0;
-		while (str[pos] == ' ')
-			pos++;
-		tmp = pos;
-		while (str[pos++] != ' ')
-			size++;
-		tmpP = malloc(sizeof(char) * (size + 1));
-		if (!tmpP)
-		{
-			free(tmpP);
-			return (NULL);
-		}
-		for (j = 0; j < size; j++)
-		{
-			tmpP[j] = str[tmp];
-			tmp++;
-		}
-		tmpP[j] = '\0';
-		a[i] = tmpP;
-	}
-	a[i] = NULL;
-	return (a);
-}
+        if (str == NULL || *str == '\0')
+                return (NULL);
+        words = word_count(str);
+        if (words == 0)
+                return (NULL);
+        a = (char **)malloc(sizeof(char *) * (words + 1));
+        if (!a)
+                return (NULL);
+        j = 0;
+        for (i = 0; i < words; i++)
+-- INSERT --                                                                                                                                                                  43,1           4%
+
