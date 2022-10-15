4034929253296034fbc2dfbe00479c348fcfccd1
Israel <alaladebamigboye@gmail.com>
2022-10-15 06:46:27 -0700

    Write a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,55 +1,27 @@
-#include <stdio.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of parameters
  *
- * Return: 0 if false, something else otherwise.
+ * Return: exit status
  */
-int is_98(int elem)
+int main(int argc, char **argv)
 {
-	return (98 == elem);
-}
+	int (*fun)(int, int);
 
-/**
- * is_strictly_positive - check if a number is greater than 0
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int is_strictly_positive(int elem)
-{
-	return (elem > 0);
-}
-
-
-/**
- * abs_is_98 - check if the absolute value of a number is 98
- * @elem: the integer to check
- *
- * Return: 0 if false, something else otherwise.
- */
-int abs_is_98(int elem)
-{
-	return (elem == 98 || -elem == 98);
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
-	int index;
-
-	index = int_index(array, 20, is_98);
-	printf("%d\n", index);
-	index = int_index(array, 20, abs_is_98);
-	printf("%d\n", index);
-	index = int_index(array, 20, is_strictly_positive);
-	printf("%d\n", index);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	fun = get_op_func(argv[2]);
+	if (!fun)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", fun(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
